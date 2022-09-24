fb478143556ffda9ca8856ccb6b7f6ce60ee7873
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-24 18:53:45 +0330

    addToCart action added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,12 @@
 import Layout from "../Layout/Layout";
 import * as data from "../data";
+import { useCartActions } from "../Providers/CartProvider";
 
 const HomePage = () => {
+  const dispatch= useCartActions();
+
   const addProductHandler=(product)=>{
-    console.log(product);
+    dispatch({type:'ADD_TO_CART',payload:product})
   }
 
   return (
