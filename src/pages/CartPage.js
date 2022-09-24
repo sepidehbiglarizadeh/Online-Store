1c29739f673155d2c85ba0162aaacb80faf4d98f
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-24 19:21:20 +0330

    load product in cartPage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,22 @@
 import Layout from "../Layout/Layout";
+import { useCart } from "../Providers/CartProvider";
 
 const CartPage = () => {
+  const { cart } = useCart();
+
   return (
     <Layout>
-      <div>This is CartPage...</div>
+      <main>
+        {cart.length ? (
+          cart.map((item) => (
+            <div key={item.id}>
+              <p>{item.name}</p>
+            </div>
+          ))
+        ) : (
+          <p>No Item In Cart</p>
+        )}
+      </main>
     </Layout>
   );
 };
