b6df0ee437eb86f56eb335e4b79bf3f12eb046fe
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-24 20:48:36 +0330

    add style to cartPage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,6 +1,6 @@
 import Layout from "../Layout/Layout";
 import { useCart } from "../Providers/CartProvider";
-import "./CartPage.css"
+import "./CartPage.css";
 
 const CartPage = () => {
   const { cart } = useCart();
@@ -18,25 +18,27 @@ const CartPage = () => {
   return (
     <Layout>
       <main className="container">
-        <section className="cartItemList">
-          {cart.map((item) => {
-            return (
-              <div className="cartItem">
-                <div className="itemImage">
-                  <img src={item.image} alt={item.name}/>
-                </div>
-                <div>{item.name}</div>
-                <div>${item.price * item.quantity}</div>
-                <div>
+        <section className="cartCenter">
+          <section className="cartItemList">
+            {cart.map((item) => {
+              return (
+                <div className="cartItem">
+                  <div className="itemImage">
+                    <img src={item.image} alt={item.name} />
+                  </div>
+                  <div>{item.name}</div>
+                  <div>${item.price * item.quantity}</div>
+                  <div>
                     <button>Remove</button>
                     <button>{item.quantity}</button>
                     <button>Add</button>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </section>
+          <section className="cartSummery">Cart Summery</section>
         </section>
-        <section className="cartSummery">Cart Summery</section>
       </main>
     </Layout>
   );
