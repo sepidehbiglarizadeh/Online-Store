bbf684db671786c40f38069f85b528254b562c59
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-24 19:47:52 +0330

    add style to cartPage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,21 +1,42 @@
 import Layout from "../Layout/Layout";
 import { useCart } from "../Providers/CartProvider";
+import "./CartPage.css"
 
 const CartPage = () => {
   const { cart } = useCart();
 
+  if (!cart.length) {
+    return (
+      <Layout>
+        <main>
+          <h2>Cart is Empty</h2>
+        </main>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <main>
-        {cart.length ? (
-          cart.map((item) => (
-            <div key={item.id}>
-              <p>{item.name}</p>
-            </div>
-          ))
-        ) : (
-          <p>No Item In Cart</p>
-        )}
+      <main className="container">
+        <section className="cartItemList">
+          {cart.map((item) => {
+            return (
+              <div className="cartItem">
+                <div className="itemImage">
+                  <img src={item.image} alt={item.name}/>
+                </div>
+                <div>{item.name}</div>
+                <div>${item.price * item.quantity}</div>
+                <div>
+                    <button>Remove</button>
+                    <button>{item.quantity}</button>
+                    <button>Add</button>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+        <section className="cartSummery">Cart Summery</section>
       </main>
     </Layout>
   );
