0817d526903cccd6759c413f87dcef4887ea60c9
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-24 17:55:48 +0330

    add context to application

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CartProvider from "./Providers/CartProvider";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/cart" component={CartPage} />
-        <Route path="/" component={HomePage}/>
-      </Switch>
+      <CartProvider>
+        <Switch>
+          <Route path="/cart" component={CartPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </CartProvider>
     </Router>
   );
 };
---
+++ src/Providers/cartReducer.js
@@ -0,0 +1,5 @@
+const cartReducer= (state,action)=>{
+
+}
+
+export default cartReducer;
