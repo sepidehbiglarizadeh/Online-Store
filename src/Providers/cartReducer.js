9325e28755f76f30d92b44066a5d95b91fee1a51
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-25 15:17:28 +0330

    separate actions in reducer

--- src/Providers/cartReducer.js
+++ src/Providers/cartReducer.js
@@ -1,40 +1,50 @@
+const addProductToCart = (state, payload) => {
+  const updatedCart = [...state.cart];
+  const updatedItemIndex = updatedCart.findIndex(
+    (item) => item.id === payload.id
+  );
+
+  if (updatedItemIndex < 0) {
+    updatedCart.push({ ...payload, quantity: 1 });
+  } else {
+    const updatedItem = { ...updatedCart[updatedItemIndex] };
+    updatedItem.quantity++;
+    updatedCart[updatedItemIndex] = updatedItem;
+  }
+
+  return { ...state, cart: updatedCart, total: state.total + payload.price };
+};
+
+const removeProductFromCart = (state, payload) => {
+  const updatedCart = [...state.cart];
+  const updatedItemIndex = updatedCart.findIndex(
+    (item) => item.id === payload.id
+  );
+  const updatedItem = { ...updatedCart[updatedItemIndex] };
+  if (updatedItem.quantity === 1) {
+    const filteredCart = updatedCart.filter((item) => item.id !== payload.id);
+    return {
+      ...state,
+      cart: filteredCart,
+      total: state.total - payload.price,
+    };
+  } else {
+    updatedItem.quantity--;
+    updatedCart[updatedItemIndex] = updatedItem;
+    return {
+      ...state,
+      cart: updatedCart,
+      total: state.total - payload.price,
+    };
+  }
+};
+
 const cartReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART": {
-      const updatedCart = [...state.cart];
-      const updatedItemIndex = updatedCart.findIndex(
-        (item) => item.id === action.payload.id
-      );
-
-      if (updatedItemIndex < 0) {
-        updatedCart.push({ ...action.payload, quantity: 1 });
-      } else {
-        const updatedItem = { ...updatedCart[updatedItemIndex] };
-        updatedItem.quantity++;
-        updatedCart[updatedItemIndex] = updatedItem;
-      }
-
-      return { ...state, cart: updatedCart ,total:state.total+action.payload.price};
-    }
-
-    case "REMOVE_PRODUCT": {
-      const updatedCart = [...state.cart];
-      const updatedItemIndex = updatedCart.findIndex(
-        (item) => item.id === action.payload.id
-      );
-      const updatedItem = { ...updatedCart[updatedItemIndex] };
-      if (updatedItem.quantity === 1) {
-        const filteredCart = updatedCart.filter(
-          (item) => item.id !== action.payload.id
-        );
-        return { ...state, cart: filteredCart ,total:state.total-action.payload.price};
-      } else {
-        updatedItem.quantity--;
-        updatedCart[updatedItemIndex] = updatedItem;
-        return { ...state, cart: updatedCart ,total:state.total-action.payload.price};
-      }
-    }
-
+    case "ADD_TO_CART":
+      return addProductToCart(state, action.payload);
+    case "REMOVE_PRODUCT":
+      return removeProductFromCart(state, action.payload);
     default:
       return state;
   }
