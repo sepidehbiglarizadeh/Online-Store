479b5e029199f12ebc68b65f90ac91c822d6c3b9
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-27 18:51:48 +0330

    redesign navigation

--- src/components/Navifation/Navigation.js
+++ src/components/Navifation/Navigation.js
@@ -3,21 +3,29 @@ import { useCart } from "../../Providers/CartProvider";
 import "./Navigation.css";
 
 const Navigation = () => {
-  const {cart}=useCart();
+  const { cart } = useCart();
   return (
     <header className="mainNavigation">
       <nav>
         <ul>
+          <div>Sepideh Shopping</div>
           <li>
-            <NavLink activeClassName="activeLink" exact to="/">Home</NavLink>
+            <NavLink activeClassName="activeLink" exact to="/">
+              Home
+            </NavLink>
           </li>
+        </ul>
+        <ul>
           <li className="cartLink">
-            <NavLink activeClassName="activeLink" to="/cart">Cart 
+            <NavLink activeClassName="activeLink" to="/cart">
+              Cart
             </NavLink>
             <span>{cart.length}</span>
           </li>
+          <li>
+            <NavLink to="/login">Log-in/sign-up</NavLink>
+          </li>
         </ul>
-        <div>Sepideh Shopping</div>
       </nav>
     </header>
   );
