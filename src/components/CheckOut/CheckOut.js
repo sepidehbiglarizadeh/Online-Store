d8586ecb9400f3cfde15c3d06f9a38718f220f2c
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-28 18:38:34 +0330

    add style to ChekoutPage

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,22 +1,49 @@
+import { Link } from "react-router-dom";
 import { useAuth } from "../../Providers/AuthProvider";
 import { useCart } from "../../Providers/CartProvider";
+import "./CheckOut";
 
 const CheckOut = () => {
   const auth = useAuth();
-  const {cart}= useCart();
+  const { cart, total } = useCart();
+
+  if (!cart.length)
+    return (
+      <main className="container">
+        <Link to="/">Got to Shopping</Link>
+      </main>
+    );
+
   return (
-    <div>
-      {auth ? (
-        <>
-          <p>name:{auth.name}</p>
-          <p>email : {auth.email}</p>
-          <p>phoneNumber: {auth.phoneNumber}</p>
-          {cart.map((item)=> <div>product name : {item.name}</div>)}
-        </>
-      ) : (
-        <p>please login</p>
-      )}
-    </div>
+    <main className="container">
+      <section className="cartCenter">
+        {auth ? (
+          <>
+            <section className="cartItemList">
+              <h3>CheckOut Detail</h3>
+              <p>Name : {auth.name}</p>
+              <p>Email : {auth.email}</p>
+              <p>Phone Number: {auth.phoneNumber}</p>
+            </section>
+            <section className="cartSummery">
+              {cart &&
+                cart.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      {item.name} * {item.quantity} :{" "}
+                      {item.quantity * item.offPrice}
+                    </div>
+                  );
+                })}
+              <hr />
+              <div>Total Price: {total}</div>
+            </section>
+          </>
+        ) : (
+          <p>Please Login</p>
+        )}
+      </section>
+    </main>
   );
 };
 
